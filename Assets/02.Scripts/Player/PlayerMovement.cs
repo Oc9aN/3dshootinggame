7ad8c963e2ef81988388816b8fef633c511f0e15
OhSiHyeon <osh000308@gmail.com>
2025-04-21 16:03:24 +0900

    Feat | 클래스명 변경 & 2단 점프

--- Assets/02.Scripts/Player/PlayerMovement.cs
+++ Assets/02.Scripts/Player/PlayerMovement.cs
@@ -2,7 +2,7 @@ using System;
 using Unity.Android.Gradle.Manifest;
 using UnityEngine;
 
-public class PlayerMove : PlayerComponent
+public class PlayerMovement : PlayerComponent
 {
     // 목표: WASD로 캐릭터를 카메라에 맞게 이동
     
@@ -10,15 +10,18 @@ public class PlayerMove : PlayerComponent
     // 1. 키보드 입력을 받는다.
     // 2. 입력으로부터 방향을 설정한다.
     // 3. 방향에 따라 플레이어를 이동한다.
+    // TODO: 달리면서 점프하면 더 높게 뛰는 것 수정
     private const float GRAVITY = -9.8f;
+    private const int MAX_JUMPCOUNT = 2;
     
     [SerializeField]
-    private float _jumpForce = 10f;
+    private float _jumpForce = 5f;
     
     private CharacterController _characterController;
     
     private float _yVelocity = 0f;
-    private bool _isJumping = false;
+    private bool _jumpAble = false;
+    private int _jumpCounter = 0;
 
     protected override void Awake()
     {
@@ -28,10 +31,19 @@ public class PlayerMove : PlayerComponent
 
     private void Update()
     {
-        Move();
+        Movement();
     }
 
-    private void Move()
+    private void Movement()
+    {
+        Vector3 direction = Move();
+        Jump();
+        direction.y = _yVelocity;
+        // 캐릭터 컨트롤러로 이동
+        _characterController.Move(direction * (Player.MoveSpeed * Time.deltaTime));
+    }
+
+    private Vector3 Move()
     {
         // 키보드 입력
         float horizontal = Input.GetAxis("Horizontal");
@@ -42,25 +54,31 @@ public class PlayerMove : PlayerComponent
         
         // 플레이어 기준으로 방향을 변환
         direction = Camera.main.transform.TransformDirection(direction);
+        
+        return direction;
+    }
 
+    private void Jump()
+    {
         if (_characterController.isGrounded)
         {
-            _isJumping = false;
+            _jumpAble = true;
+            _jumpCounter = 0;
         }
         
         // 점프
-        if (Input.GetKeyDown(KeyCode.Space) && _isJumping == false)
+        if (Input.GetKeyDown(KeyCode.Space) && _jumpAble)
         {
             _yVelocity = _jumpForce;
-
-            _isJumping = true;
+            _jumpCounter++;
+            if (_jumpCounter >= MAX_JUMPCOUNT)
+            {
+                // 더 많이 뛰면 점프 막기
+                _jumpAble = false;
+            }
         }
         
         // 중력 적용
         _yVelocity += GRAVITY * Time.deltaTime;
-        direction.y = _yVelocity;
-
-        // 캐릭터 컨트롤러로 이동
-        _characterController.Move(direction * (Player.MoveSpeed * Time.deltaTime));
     }
 }
