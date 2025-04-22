cae598f9b814e63ef850607bc780919513c73ea7
OhSiHyeon <osh000308@gmail.com>
2025-04-22 13:45:39 +0900

    ADD | 에셋 추가

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -5,9 +5,14 @@ public class PlayerFire : MonoBehaviour
 {
     // 마우스 왼쪽과 오른쪽으로 총알과 수류탄을 발사
 
+    [SerializeField]
     private GameObject _firePosition;
 
+    [SerializeField]
     private GameObject _bombPrefab;
+    
+    [SerializeField]
+    private ParticleSystem _bulletEffect;
 
     private void Update()
     {
@@ -20,6 +25,18 @@ public class PlayerFire : MonoBehaviour
             Rigidbody bombRigidbody = bomb.GetComponent<Rigidbody>();
             bombRigidbody.AddForce(Camera.main.transform.forward * 15f, ForceMode.Impulse);
             bombRigidbody.AddTorque(Vector3.one);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                _bulletEffect.transform.position = hit.point;
+                _bulletEffect.transform.forward = hit.normal;
+                _bulletEffect.Play();
+            }
         }
     }
 }
