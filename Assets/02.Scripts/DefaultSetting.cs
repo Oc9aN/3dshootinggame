441ed07a59a54c69222061bbd63cf9bcae5b396a
OhSiHyeon <osh000308@gmail.com>
2025-04-22 13:45:24 +0900

    Feat | 커서 표시 비활성

---
+++ Assets/02.Scripts/DefaultSetting.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+public class DefaultSetting : MonoBehaviour
+{
+    private void Awake()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
