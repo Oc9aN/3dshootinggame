cd97764584d5c493e0741181b976314e0c5fd050
OhSiHyeon <osh000308@gmail.com>
2025-04-24 15:31:38 +0900

    Feat | 커서 설정

--- Assets/02.Scripts/Utility/DefaultSetting.cs
+++ Assets/02.Scripts/Utility/DefaultSetting.cs
@@ -3,9 +3,32 @@ using UnityEngine;
 
 public class DefaultSetting : MonoBehaviour
 {
-    private void Awake()
+    bool isUIOpen = false;
+
+    private void Start()
     {
-        // Cursor.visible = false;
-        // Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isUIOpen = !isUIOpen;
+
+            if (isUIOpen)
+            {
+                // UI 열기
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else
+            {
+                // UI 닫고 게임 재개
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
     }
 }
