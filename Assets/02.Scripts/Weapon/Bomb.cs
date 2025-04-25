5783e500b3e0964fd948910416c0308629a5368c
OhSiHyeon <osh000308@gmail.com>
2025-04-25 11:12:23 +0900

    Feat | 플레이어 폭탄

--- Assets/02.Scripts/Weapon/Bomb.cs
+++ Assets/02.Scripts/Weapon/Bomb.cs
@@ -1,10 +1,16 @@
 using System;
 using UnityEngine;
 
-public class Bomb : MonoBehaviour, IPoolObject
+public class Bomb : MonoBehaviour, IPoolObject, IExplodable
 {
     [SerializeField]
     private GameObject _explosionEffectPrefab;
+
+    [SerializeField]
+    private float _explodeRange;
+    
+    [SerializeField]
+    private Damage _explodeDamage ;
 
     private Rigidbody _rigidbody;
     
@@ -24,14 +30,30 @@ public class Bomb : MonoBehaviour, IPoolObject
 
     private void OnCollisionEnter(Collision collision)
     {
-        GameObject effectObject = Instantiate(_explosionEffectPrefab);
-        effectObject.transform.position = transform.position;
-        
-        Pool_Bomb.Instance.ReturnPooledObject(this);
+        Explode();
     }
 
     public void Initialize()
     {
         
+    }
+
+    public void Explode()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, _explodeRange);
+        foreach (var hitCollider in hitColliders)
+        {
+            if (hitCollider.TryGetComponent(out IDamageable damageableObject))
+            {
+                _explodeDamage.From = gameObject;
+                // 데미지를 입는 객체인 경우
+                damageableObject.TakeDamage(_explodeDamage);
+            }
+        }
+        
+        GameObject effectObject = Instantiate(_explosionEffectPrefab);
+        effectObject.transform.position = transform.position;
+        
+        Pool_Bomb.Instance.ReturnPooledObject(this);
     }
 }
