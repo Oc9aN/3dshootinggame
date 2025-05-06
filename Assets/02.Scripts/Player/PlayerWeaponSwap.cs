55caacfa92aa9409a15cc48366bb78d4ca7bc3ae
Oc9aN <59911975+Oc9aN@users.noreply.github.com>
2025-05-06 14:04:16 +0900

    Feat | 무기 휠로 변경

--- Assets/02.Scripts/Player/PlayerWeaponSwap.cs
+++ Assets/02.Scripts/Player/PlayerWeaponSwap.cs
@@ -8,14 +8,54 @@ public class PlayerWeaponSwap : PlayerComponent
     [SerializeField]
     private Transform _weaponTransform;
 
+    private int _currentWeaponIndex = 1;
+
+    private int CurrentWeaponIndex
+    {
+        get => _currentWeaponIndex;
+        set
+        {
+            _currentWeaponIndex = value;
+            SwapHandler(_currentWeaponIndex);
+        }
+    }
+
     private void Start()
     {
-        SwapWeapons(0);
+        CurrentWeaponIndex = 1;
     }
 
     private void Update()
     {
         if (InputHandler.GetKeyDown(KeyCode.Alpha1))
+        {
+            CurrentWeaponIndex = 1;
+        }
+        else if (InputHandler.GetKeyDown(KeyCode.Alpha2))
+        {
+            CurrentWeaponIndex = 2;
+        }
+        else if (InputHandler.GetKeyDown(KeyCode.Alpha3))
+        {
+            CurrentWeaponIndex = 3;
+        }
+        
+        // 마우스 휠
+        float scroll = InputHandler.GetAxis("Mouse ScrollWheel");
+
+        if (scroll > 0f)
+        {
+            CurrentWeaponIndex = Mathf.Min(CurrentWeaponIndex + 1, 3);
+        }
+        else if (scroll < 0f)
+        {
+            CurrentWeaponIndex = Mathf.Max(CurrentWeaponIndex - 1, 1);
+        }
+    }
+
+    private void SwapHandler(int index)
+    {
+        if (index == 1)
         {
             // 무기에 맞는 애니메이션 트리거를 액션으로 지정
             SwapWeapons(EWeaponType.Rifle);
@@ -24,17 +64,17 @@ public class PlayerWeaponSwap : PlayerComponent
             Player.Animator.SetLayerWeight(Player.Animator.GetLayerIndex("Melee Layer"), 0);
             Player.Animator.SetLayerWeight(Player.Animator.GetLayerIndex("Melee Mask"), 0);
         }
-        else if (InputHandler.GetKeyDown(KeyCode.Alpha2))
-        {
-            SwapWeapons(EWeaponType.Pistol);
-        }
-        else if (InputHandler.GetKeyDown(KeyCode.Alpha3))
+        else if (index == 2)
         {
             SwapWeapons(EWeaponType.Bat);
             Player.Animator.SetLayerWeight(Player.Animator.GetLayerIndex("Rifle Layer"), 0);
             Player.Animator.SetLayerWeight(Player.Animator.GetLayerIndex("Rifle Mask"), 0);
             Player.Animator.SetLayerWeight(Player.Animator.GetLayerIndex("Melee Layer"), 1);
             Player.Animator.SetLayerWeight(Player.Animator.GetLayerIndex("Melee Mask"), 1);
+        }
+        else if (index == 3)
+        {
+            SwapWeapons(EWeaponType.Pistol);
         }
     }
 
