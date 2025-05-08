f0f5c636c2d43a20fb3681a976d019811fdd0685
OhSiHyeon <osh000308@gmail.com>
2025-05-08 09:48:44 +0900

    Fix | 쿼터뷰 버그 수정

--- Assets/02.Scripts/Player/ViewStrategy/CrosshairRotationStrategy.cs
+++ Assets/02.Scripts/Player/ViewStrategy/CrosshairRotationStrategy.cs
@@ -2,16 +2,15 @@ using UnityEngine;
 
 public class CrosshairRotationStrategy : IRotationStrategy
 {
-    private float _rotationX = 0f;
-    
     public float RotationAngle(Player player)
     {
         // 1. 마우스 입력을 받는다.
         float mouseX = InputHandler.GetAxis("Mouse X");
         
         // 2. 마우스 입력으로부터 회전 시킬 크기를 누적한다.
-        _rotationX += mouseX * player.Data.RotateSpeed * Time.deltaTime;
+        float yRotation = player.transform.eulerAngles.y;
+        yRotation += mouseX * player.Data.RotateSpeed * Time.deltaTime;
 
-        return _rotationX;
+        return yRotation;
     }
 }
